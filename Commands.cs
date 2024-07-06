6ceb97339f4b599f1e797682456e39f9d19a2e11
EltonZhang777 <280903158@qq.com>
2024-07-06 16:07:14 +0800

    Initial commit

---
+++ Commands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Blasphemous.CheatConsole;
+using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.AtriumOfAtonement;
+
+internal class Command_BossFight : ModCommand
+{
+
+}
---
+++ Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.AtriumOfAtonement;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+public class Main : BaseUnityPlugin
+{
+    public static AtriumOfAtonement AtriumOfAtonement { get; private set; }
+
+    private void Start()
+    {
+        AtriumOfAtonement = new AtriumOfAtonement();
+    }
+}
